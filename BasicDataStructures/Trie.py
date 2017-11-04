4b4c7257e6a78e7261f7eed7828ee14cf0f0be47
Alexander Morsey <alexmorsey@gmail.com>
2017-11-03 21:32:19 -0700

    Worked on search funciton, still needs a lot of work

--- BasicDataStructures/Trie.py
+++ BasicDataStructures/Trie.py
@@ -24,15 +24,47 @@ class Trie:
         pass
 
     def search(self, word):
-        pass
+        p = self.root
+        i = 0
+        length = len(word)
+        while i < length and word[i] in p.children:
+            p = p.children[word[i]]
+        if i == length:
+            return p
+        else:
+            return None
+
+    def children(self, parent, prefix):
+        l = []
+        for i in parent.children:
+            a = children(parent.children[i])
+            for e in a:
+                l.append(prefix+e)
+        return l
+
 
     def prefix(self, word):
-        pass
+        p = self.search(word)
+        words = []
+
+        if p:
+            for c in p.children:
+                pass
+
+
+myDict = {"a": "1", "c": "2", "f": "3"}
+for c in myDict:
+    print(myDict[c])
+
+
 
 myTrie = Trie()
+myTrie.add("the")
+myTrie.add("he")
+myTrie.add("she")
+myTrie.add("they")
+myTrie.add("them")
+myTrie.add("heath")
+myTrie.add("see")
 
-s = "alex"
-
-for l in s:
-    print(l)
 
