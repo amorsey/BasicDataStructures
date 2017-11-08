edae99f7c0f5c26235f4dd71695a43bfcdaf7e6c
Alexander Morsey <alexmorsey@gmail.com>
2017-11-07 21:26:23 -0800

    Made some changes. Doesn't work yet.

--- BasicDataStructures/algorithms.py
+++ BasicDataStructures/algorithms.py
@@ -1,21 +1,27 @@
 # Binary Search class
 # Finds and returns an item in an ordered list in O(log(n)) time.
 
-class algorithms:
-    def __init__(self, orderedList, desiredElement):
-        searchSpace = len(orderedList)/2
-        sel.guess = searchSpace
+class Algorithms:
+    def binarySearch(self, orderedList, desiredElement):
+        searchSpace = len(orderedList)//2
+        guess = searchSpace
 
-        while searchSpace > 1:
+        while searchSpace > 0:
+            #print(searchSpace)
             value = orderedList[guess]
-            searchSpace = searchSpace/2
+            searchSpace = searchSpace//2
             if value == desiredElement:
-                return guess
+                return value
             elif value < desiredElement:
                 guess += searchSpace
             else:
                 guess -= searchSpace
 
+            #print(guess)
+            #print(searchSpace)
+            print(value)
 
-myList = [1, 4, 5, 6, 11, 13, 15, 16, 17, 22, 31, 32]
-mySearch = BinarySearch()
+
+myList = [1, 4, 5, 6, 11, 13, 15, 16, 17, 22, 31]
+alg = Algorithms()
+print(alg.binarySearch(myList, 17))
