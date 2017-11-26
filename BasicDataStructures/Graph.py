70c09b92bbf4067f2c1b33e44a8de82663c40a07
Alexander Morsey <alexmorsey@gmail.com>
2017-11-26 14:55:38 -0800

    Added dfs, and bfs

--- BasicDataStructures/Graph.py
+++ BasicDataStructures/Graph.py
@@ -1,9 +1,34 @@
+from collections import deque
+
+
 class Graph:
     def __init__(self):
         self.adjacent = {}
 
     def add(self, data, children):
         self.adjacent[data] = children
+
+    def bfs(self, start):
+        visited = {start}
+        queue = deque([start])
+        while len(queue) > 0:
+            print(queue)
+            node = queue.popleft()
+            for child in self.adjacent[node]:
+                if child not in visited:
+                    visited.add(child)
+                    queue.append(child)
+
+    def dfs(self, start):
+        visited = set()
+        self.dfs_helper(visited, start)
+
+    def dfs_helper(self, visited, parent):
+        visited.add(parent)
+        print(parent)
+        for child in self.adjacent[parent]:
+            if child not in visited:
+                self.dfs_helper(visited, child)
 
     def __str__(self):
         s = ""
@@ -18,9 +43,9 @@ my_graph = Graph()
 my_graph.add('A',['B'])
 my_graph.add('B',['A','C','D'])
 my_graph.add('C',['B','D'])
-my_graph.add('D',['E','G','H'])
+my_graph.add('D',['B','C','E','G','H'])
 my_graph.add('E',['D','F'])
 my_graph.add('F',['E','G'])
 my_graph.add('G',['D','F'])
 my_graph.add('H',['D'])
-print(my_graph)
+my_graph.dfs('E')
